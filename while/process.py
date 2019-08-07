564d68c7a14b8e7986ef2dda6050c7da0e3ae172
owhileo <773746226@qq.com>
2019-08-07 10:37:10 +0800

    new filter added

--- while/process.py
+++ while/process.py
@@ -5,6 +5,10 @@
 
 import json
 
+origin_json_path = r'data4.json'
+save_json_path = r'data4.json'
+type_delete_path = r'types_delete.txt'
+
 
 def read_json(path):
     with open(path, 'r', encoding='utf-8') as f:
@@ -22,7 +26,7 @@ def save_json(path, data):
 # 最后返回修改过的data
 # 写规则的时候记得加一条 y['label'] == 9 对未标定的应用这条 ***
 def type_filter(data):
-    with open(r'types_delete.txt', 'r', encoding='utf-8') as f:
+    with open(type_delete_path, 'r', encoding='utf-8') as f:
         type_drop = [x[:-1] for x in f.readlines()]  # 可能是linux特性？直接读每行末尾有\n,故虑去最后一个字符
     for x in data:
         for y in x['records']:
@@ -32,14 +36,17 @@ def type_filter(data):
                 print('type_filter find:' + y['type'] + ' and label it:' + '0')
     return data
 
+
 def type_filter_new1(data):
+    new_type_delete = ['一般:客户付款', "喷漆:客户付款", "服务节免费检查","召回活动"]
     for x in data:
         for y in x['records']:
-            if y['type'] =='一般:客户付款' and y['label'] == 9:
+            if y['type'] in new_type_delete and y['label'] == 9:
                 y['label'] = 0
                 y['reason'] = 'type过滤:' + y['type']
                 print('self added type_filter find:' + y['type'] + ' and label it:' + '0')
     return data
+
 
 def short_filter(data):
     # with open(r'E:\CS\Git_repo\car300\types_delete.txt', 'r', encoding='utf-8') as f:
@@ -61,10 +68,10 @@ def short_filter(data):
     return data
 
 
-filters = [type_filter, short_filter,type_filter_new1]
+filters = [type_filter, short_filter, type_filter_new1]
 
 if __name__ == '__main__':
-    dat = read_json(r'data4.json')
+    dat = read_json(origin_json_path)
     for i in filters:
         dat = i(dat)
-    save_json(r'data4.json', dat)
+    save_json(save_json_path, dat)
