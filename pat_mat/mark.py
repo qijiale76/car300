1c834f55ae7f09fa947e692ddbaaed264ca38a05
Qi Jiale <171850589@smail.nju.edu.cn>
2019-08-07 11:51:59 +0800

    mark with highlight

--- pat_mat/mark.py
+++ pat_mat/mark.py
@@ -3,9 +3,12 @@
 # Date: 2019-8-7
 # Version: 1.0
 
-import json
+import json,sys
 
-filename="data3_new.json"
+filename="data3.json"
+
+high1=["纵梁","车顶","避震器","防火墙","A柱","B柱","C柱","气囊","备胎室","泡水","火烧","水泡","叶子板","翼子板","前叶","后叶"]
+#high2=["切割","钣金","焊接",]
 
 def read_json(path):
     with open(path, 'r') as f:
@@ -19,21 +22,31 @@ def save_json(path, data):
 
 
 def mark(data):
-    for x in data:
-        for y in x['records']:
+    for ix,x in enumerate(data):
+        for iy,y in enumerate(x['records']):
             if y['label']==9:
+                print(str(iy+1)+"/"+str(len(x['records']))+"   "+str(ix+1)+"/"+str(len(data)))
                 print("type:"+y["type"])
-                print("detail:"+y["detail"])
-                print("other"+y["other"])
+                det=y["detail"]
+                oth=y["other"]
+                for k in high1:
+                    det=det.replace(k,'\033[1;32;40m'+k+'\033[0m')
+                    oth=oth.replace(k,'\033[1;32;40m'+k+'\033[0m')
+                print("detail:"+det)
+                print("other:"+oth)
+                mylabel=input()
                 if mylabel=="":
                     mylabel=0
-                mylabel=int(input())
+                elif mylabel=="exit":
+                    save_json(filename, data)
+                    exit()
+                mylabel=int(mylabel)
                 if(mylabel<0 or mylabel >2):
                     print("wrong label, this record passed")
                 else:
                     y['label']=mylabel
-                    save_json(filename, data)
                     print("label "+str(mylabel)+" success.")
+                    print("")
 
     # #sum_s = 0
     # #labeled = 0
@@ -52,5 +65,8 @@ def mark(data):
 
 
 if __name__ == '__main__':
+    if(sys.version[0]!="3"):
+        print("use python3 pls!")
+        exit()
     dat = read_json(filename)
     mark(dat)
